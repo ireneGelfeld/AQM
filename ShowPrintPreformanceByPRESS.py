60c7bf200b5262c574a2d835f53507dd89e10841
ireneGelfeld <42513551+ireneGelfeld@users.noreply.github.com>
2024-01-11 14:03:37 +0200

    Update ShowPrintPreformanceByPRESS.py

--- ShowPrintPreformanceByPRESS.py
+++ ShowPrintPreformanceByPRESS.py
@@ -11,14 +11,19 @@ import plotly.graph_objects as go
 from plotly.offline import download_plotlyjs, init_notebook_mode,  plot
 from plotly.subplots import make_subplots
 import plotly.express as px
+import math
 
 ##############################
-
+global dataPracentage
 bins=10
-Name_of_columns_4_hist=['meanC2C_Front', 'stdC2C_Front', 'percentile_95_C2C_Front',
-       'meanC2C_Back', 'stdC2C_Back', 'percentile_95_C2C_Back'
-       ]
+Name_of_columns_4_hist={'meanC2C_Front':1,
+                        'stdC2C_Front':0,
+                        'percentile_95_C2C_Front':1,
+                        'meanC2C_Back':1,
+                        'stdC2C_Back':0,
+                        'percentile_95_C2C_Back':1}
 
+dataPracentage=95 #Extreme data to filter 95%
 
 ###############################
 
@@ -50,6 +55,25 @@ def load_csvs_and_plot_histogram(directory, string_in_filename):
     concatenated_df = pd.concat(df_list, ignore_index=True)
     
     return  concatenated_df   
+
+def FilterFromDataExtremVals(concatenated_df):
+    
+        concatenated_df_fitered=pd.DataFrame() 
+        for col in concatenated_df.columns:
+            
+            try:
+                 percentile_x_1 = np.percentile(concatenated_df[col], dataPracentage)
+                 percentile_1 = np.percentile(concatenated_df[col], 100-dataPracentage)
+                
+                 inx2copy = [i for i, x in enumerate(concatenated_df[col]) if x >= percentile_1 and x <= percentile_x_1]
+                 if math.isnan(percentile_1):
+                     continue;
+                 
+                 concatenated_df_fitered=pd.concat([concatenated_df_fitered,concatenated_df[col][inx2copy].reset_index(drop=True)],axis=1)
+            except:
+                 continue;
+        return concatenated_df_fitered                     
+
     
 def PlotHistogram(concatenated_df,colName,bins):  
     # Plot histogram using Plotly graph_objects
@@ -79,9 +103,12 @@ string_in_filename = 'C2C_pressOverview_'
 # load_csvs_and_plot_histogram(directory_path, string_to_search)
 concatenated_df= load_csvs_and_plot_histogram(directory,string_in_filename)
 
+concatenated_df_fitered= FilterFromDataExtremVals(concatenated_df)
 
-for itm in Name_of_columns_4_hist:
+
+for key,value in Name_of_columns_4_hist.items():
     try:
-        PlotHistogram(concatenated_df,itm,bins)
+        if value:
+            PlotHistogram(concatenated_df_fitered,key,bins)
     except:
         continue;

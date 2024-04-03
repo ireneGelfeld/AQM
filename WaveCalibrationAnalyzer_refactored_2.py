a7f8eafcad244e291ae9e9c3d3d8a8596bb64b03
yoavgeva2 <116574866+yoavgeva2@users.noreply.github.com>
2024-04-03 15:06:00 +0300

    י
    י

--- WaveCalibrationAnalyzer_refactored_2.py
+++ WaveCalibrationAnalyzer_refactored_2.py
@@ -16,9 +16,9 @@ global StartCycle,StartCycle4Avr,PHpoitToIgnor,MaxWaveWindow,DistanceBtWPointMM,
 
 
 ## for plot per panel and plot per cycle and WaveData SUB Average_PerPanel_PerCycle
-plotPerPanel=1;# On/OFF plot
+plotPerPanel=0;# On/OFF plot
 plotPerCycle=0;## On/OFF plot
-WaveDataSUBAverage_PerPanel_PerCycle=1 # On/OFF plot (Avi method)
+WaveDataSUBAverage_PerPanel_PerCycle=0 # On/OFF plot (Avi method)
 CycleNumber =3 # cycle view in => plot Per Panel
 StartCycle4Avr = 2; # Start averaging for all plots defult = 2
 Panel = 6;          #view panel for plot Per cycle
@@ -31,7 +31,7 @@ colorPNL=px.colors.sequential.Reds[2:]+px.colors.sequential.Viridis;
 
 
 ## for plot CIScurve
-CIScurve=1;# On/OFF plot
+CIScurve=0;# On/OFF plot
 
 ## for plot registration estimation in Wave Prints (yuval)
 registrationBetweenWavePrints=0; # On/OFF plot ERROR
@@ -47,14 +47,14 @@ BeforAndAfterCorr=1# On/OFF plot
 ## DX plot - delta between wave and starvitzky filer (residue) 
 WaveFilterResidue_dxPlot=1 # On/OFF plot
 PHpoitToIgnor=2; # Ponits of Print head to ignar (16 point in total) in each side
-MaxWaveWindow=51;# S.gol filter window
-S_g_Degree=1;# S.gol filter degree
+MaxWaveWindow=121;# S.gol filter window
+S_g_Degree=3;# S.gol filter degree
 DistanceBtWPointMM=2.734
 NieghborColorsFor7colrs=6# parameter for distortion correction (number of nighboring colors)
 
 
 ###for Tables
-PlotTables=1# On/OFF table
+PlotTables=0# On/OFF table
 ColorLevels= 5; # Heat Map for offset- number of levels of colors from white to hot red
 DivideByNum= 20; # Correction for offset Haet map- if occurs error try to increase this number
 ColorLevelsTilt=7; #Heat Map for tilt- number of levels of colors from white to hot red
